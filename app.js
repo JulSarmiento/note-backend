00e22fcce9d2a3e49673346527ad36c5d383b2b9
JulSarmiento <jasa1999@hotmail.com>
2023-05-16 13:48:16 -0500

    updating initial config

--- app.js
+++ app.js
@@ -8,15 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logger('development'));
 
-// Static files
-app.use(express.static('./src/public'));
-
-// View engine setup
-app.set('view engine', 'ejs');
-app.set('views', './src/views');
-
 // Routes
-app.use('/', indexRouter);
+app.use(indexRouter);
 
 app.use(errorHandler);
 app.use(pageNotFound);
